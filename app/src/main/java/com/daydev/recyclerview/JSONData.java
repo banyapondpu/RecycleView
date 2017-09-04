a5e0fcc8699d441957961be2d51b6f37870e8d9c
Banyapon Poolsawad <daydev@Banyapons-MacBook-Pro.local>
2017-09-04 13:39:07 +0700

    push all

---
+++ app/src/main/java/com/daydev/recyclerview/JSONData.java
@@ -0,0 +1,26 @@
+package com.daydev.recyclerview;
+
+/**
+ * Created by daydev on 9/3/17.
+ */
+
+public class JSONData {
+    private String title;
+    private String thumbnail;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
---
+++ app/src/main/java/com/daydev/recyclerview/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.daydev.recyclerview;
+
+/**
+ * Created by daydev on 9/3/17.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(JSONData item);
+}
